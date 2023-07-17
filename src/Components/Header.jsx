21fb6e28b588d47c17a339a1cb74e3816ef83f0e
WafaBenyettou <benwafa951@gmail.com>
2023-07-17 07:09:26 +0100

    Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import logo from "../assets/img/logo.png";
+import { IoLogoYoutube } from "react-icons/io5";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center">
-      <img src={logo} alt="logo"className="w-[100px]" />
+    <div className="flex justify-center">
+      <div className="flex justify-between items-center max-w-5xl w-full px-4">
+        <img src={logo} alt="logo" className="w-[100px]" />
 
-     <ul className="flex gap-4 md:gap-14">
-        <li className="hover:font-bold cursor-pointer">
-            <a href="#">Home</a>
-        </li>
-        <li className="hover:font-bold cursor-pointer">
-            <a href="#">About</a>
-        </li>
-        <li className="hover:text-black font-bold cursor-pointer">
-            <a href="#">Contact</a>
-        </li>
-    
-     </ul>
+        <ul className="hidden md:flex gap-4 md:gap-14">
+          <li className="hover:font-bold cursor-pointer">Home</li>
+          <li className="hover:font-bold cursor-pointer">About</li>
+          <li className="hover:font-bold cursor-pointer">Contact</li>
+        </ul>
 
-     <button className="bg-red-500 rounded-full text-white">Sign Up</button>
+        <button className="bg-blue-500 rounded-full text-white flex items-center text-[14px]">
+          Subscribe <IoLogoYoutube className="w-6 h-6 ml-3" />
+        </button>
+      </div>
     </div>
   );
 }
