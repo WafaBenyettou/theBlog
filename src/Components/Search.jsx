4c76a29e1e5e3cf1fabe1fa49a4ecd5d4ee90210
WafaBenyettou <benwafa951@gmail.com>
2023-07-17 07:49:04 +0100

    Search

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import banner from "../assets/img/banner.png";
-
 import { IoSearchOutline } from "react-icons/io5";
 
 function Search() {
+  const tags = [
+    {
+      id: 1,
+      name: "All",
+    },
+    {
+      id: 2,
+      name: "React",
+    },
+    {
+      id: 3,
+      name: "React Native",
+    },
+    {
+      id: 4,
+      name: "Angular",
+    },
+    {
+      id: 5,
+      name: "UI/UX",
+    },
+  ];
+
+
+  const [activeIndex,setActiveIndex]=useState(0);
   return (
     <div className="flex justify-center mt-10 flex-col px-[70] md:px-[50px]">
       <img src={banner} alt="banner" className="banner rounded-2xl " />
@@ -18,6 +42,21 @@ function Search() {
           placeholder="Search"
           className="search outline-none ml-2 w-full"
         />
+      </div>
+      <div className='flex gap-10 justify-center
+       mt-5'>
+        {tags.map((item,index) => (
+          <ul  className={`${index==activeIndex?
+            'bg-blue-500 text-white':null} 
+            p-1 pb-2 rounded-sm
+            md:rounded-full cursor-pointer md:px-4
+            hover:scale-110 hover:border-[1px] 
+            border-blue-500 transition-all duration-100 ease-in-out`}>
+            <li className='line-clamp-1'>
+              {item.name}
+            </li>
+          </ul>
+        ))}
       </div>
     </div>
   );
