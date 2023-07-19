68d211354deb3eff8919953c09db33bf06c08dac
WafaBenyettou <benwafa951@gmail.com>
2023-07-19 22:56:42 +0100

    rename

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="bg-gray-200 text-center mt-20 p-10">
       <h1 className="text-[15px] pb-5 ">
-       for more information :  Contacte@theBlog.com
+       for more information : Contacte@theBlog.com
       
       </h1>
       <span className="text-blue-500">Copyright © 2023 TheBlog</span>
