ce73adc0e432470c7e2293e684d2b40db8e3c531
WafaBenyettou <benwafa951@gmail.com>
2023-07-20 16:32:04 +0100

    GlobalAPI

--- src/Services/GlobalAPI.jsx
+++ src/Services/GlobalAPI.jsx
@@ -6,6 +6,11 @@ const getPost = () => {
   return axios.get(BASE_URL + '/blogs?populate=*');
 };
 
+const getPostById = (id) => {
+  return axios.get(BASE_URL + '/blogs/'+id+'?populate=*');
+};
+
 export default {
   getPost,
+  getPostById
 };
