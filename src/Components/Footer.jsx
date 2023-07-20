51bfe1232d5bf3f40b21b48bc0cc0b7f19215c2d
WafaBenyettou <benwafa951@gmail.com>
2023-07-20 15:35:20 +0100

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-gray-200 text-center mt-20 p-10">
+    <div className="bg-gray-200 text-center mt-20 p-10 rounded-2xl">
       <h1 className="text-[15px] pb-5 ">
-       for more information : Contacte@theBlog.com
+       For more information contact us at : custom-services@theBlog.com
       
       </h1>
       <span className="text-blue-500">Copyright © 2023 TheBlog</span>
