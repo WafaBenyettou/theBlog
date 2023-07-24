23c0e385c3790c83cb45e49a739949555656b815
WafaBenyettou <benwafa951@gmail.com>
2023-07-24 23:47:34 +0100

    About

---
+++ src/Pages/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="bg-white p-8 shadow-md rounded-lg">
+      <h2 className="text-3xl font-bold mb-4">About Us</h2>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget suscipit ipsum. Vivamus
+        ut justo justo. Sed sed velit nec purus feugiat ullamcorper vitae id mauris. Duis ultrices
+        odio ac erat blandit, vel varius orci facilisis.
+      </p>
+      <p className="text-lg mt-4">
+        Phasellus eu tortor et elit euismod porta. Integer luctus vitae odio et venenatis. Nam
+        aliquet ipsum vel ipsum eleifend, quis interdum arcu dictum. Sed et luctus odio, ac
+        lobortis turpis. Sed nec odio arcu.
+      </p>
+      <p className="text-lg mt-4">
+        Proin dapibus neque et fermentum porttitor. Sed quis dui a mauris aliquet fringilla sit
+        amet ut odio. Nam rhoncus tellus vel metus euismod, ac vestibulum elit dapibus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
